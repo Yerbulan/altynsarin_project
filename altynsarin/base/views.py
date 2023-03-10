3366c635fe18323871a8b3426868c69044625b8a
Yerbulan <bula136@gmail.com>
2023-03-10 17:13:29 +0600

    Day 5

--- altynsarin/base/views.py
+++ altynsarin/base/views.py
@@ -70,13 +70,16 @@ def home(request):
 
     topic = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context =  {'rooms': rooms, 'topics': topic, 'room_count': room_count}
+
+    context =  {'rooms': rooms, 'topics': topic, 'room_count': room_count,
+                'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
 
     if request.method == "POST":
@@ -91,6 +94,15 @@ def room(request,pk):
     context = {'room': room, 'room_messages':room_messages,
                'participants':participants}
     return render(request, 'base/room.html', context)
+
+def userProfile(request,pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_message = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms, 'room_message':room_message,
+               'topics': topics}
+    return render(request, 'base/profile.html', context)
 
 @login_required(login_url = 'login')
 def createRoom(request):
